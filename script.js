d053365a549ad89b46a88636798d8cc1aba314aa
FelixvL <digitaleservice@gmail.com>
2022-11-21 09:51:48 +0100

    structuur gemaakt van bestanden

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function fillHeader() {
     document.getElementById("headerN").innerHTML = `
     <img src="#" alt="" width="39px" height="39">
         <h1>Netflix & Grill</h1>
-    
+        <br>
+        <a href="../question/Questions.html">questions</a>
+        <a href="../login/login.html">login</a>
+        <a href="../landing/landing.html">landing</a>
+        <a href="../filtering/filtering.html">filtering</a>
+        <a href="../result/result.html">result</a>
     `;
 }
 
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover();   
-  });
